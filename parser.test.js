45bec2fa718561c75a001e7851c55a51a863920e
Olof Bjarnason <olof.bjarnason@gmail.com>
2022-06-17 14:53:54 +0200

    Test needs expression

--- parser.test.js
+++ parser.test.js
@@ -10,4 +10,8 @@ describe("parser", () => {
         const result = parse("Goal\n  This is the goal\n")
         expect(result).toStrictEqual([{'goal': "This is the goal"}])
     })
+    it("can parse a needs node", () => {
+        const result = parse("Goal needs AddCalls\n  Call getScore function in both places\n")
+        expect(result).toMatchSnapshot()
+    })
 })
