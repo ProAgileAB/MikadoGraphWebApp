fe92bb651a2f4c3c8f93620790faf05065395c83
Olof Bjarnason <olof.bjarnason@gmail.com>
2022-06-17 14:47:35 +0200

    Test Goal parse

--- parser.test.js
+++ parser.test.js
@@ -6,4 +6,8 @@ describe("parser", () => {
         const result = parse("Done: Goal")
         expect(result).toStrictEqual([{'done': "Goal"}])
     })
+    it("can parse a single Goal", () => {
+        const result = parse("Goal\n  This is the goal\n")
+        expect(result).toStrictEqual([{'goal': "This is the goal"}])
+    })
 })
