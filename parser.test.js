c0da65575e0d533b99eb5ac8edccff6021869c10
Olof Bjarnason <olof.bjarnason@gmail.com>
2022-06-17 14:56:17 +0200

    Test realistic Mikado Graph expressions

--- parser.test.js
+++ parser.test.js
@@ -14,4 +14,23 @@ describe("parser", () => {
         const result = parse("Goal needs AddCalls\n  Call getScore function in both places\n")
         expect(result).toMatchSnapshot()
     })
+    it("can parse a realistic example", () => {
+        const result = parse("Goal\n" +
+            "   Reduce duplication of switch logic\n" +
+            "Goal needs Calls\n" +
+            "   Call getpointName(int) replacing both switches\n" +
+            "Calls needs AddFunction\n" +
+            "  Add getPointName(int) function based on bottom switch\n" +
+            "AddFunction needs AddAll\n" +
+            "  Add \"+All\" after top switch\n" +
+            "AddAll needs Format\n" +
+            "  Format file for readability\n" +
+            "AddAll needs DeuceReturn\n" +
+            "  Return \"Deuce if score >= before top switch\n" +
+            "Format, DeuceReturn needs Gitignore\n" +
+            "  Update .gitignore to reduce workspace clutter\n" +
+            "Done: AddAll\n" +
+            "Done: Format\n")
+        expect(result).toMatchSnapshot()
+    })
 })
