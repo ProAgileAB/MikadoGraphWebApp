81e52bf49847110b35b0d5c9ec4b87bab764ad18
Olof Bjarnason <olof.bjarnason@gmail.com>
2022-06-20 15:03:44 +0200

    Remove unneeded code elements

--- index.js
+++ index.js
@@ -12,24 +12,3 @@ async function getFile() {
         return
     }
 }
-async function getDirectory() {
-    let dirHandle = await window.showDirectoryPicker()
-    console.log(dirHandle)
-    for await (const [key, value] of dirHandle.entries()) {
-        console.log({ key, value })
-        let fileData = await value.getFile()
-        let text = await fileData.text()
-        console.log(text)
-    }
-}
-function dragOverHandler(ev) {
-    ev.dataTransfer.dropEffect = "copy"
-    console.log('File(s) in drop zone');
-    ev.preventDefault();
-}
-function dropHandler(ev) {
-    console.dir(ev)
-    console.dir(ev.dataTransfer.items[0].getAsFile())
-    console.log('File(s) dropped');
-    ev.preventDefault();
-}
