4a45fb3fea72c70c842396cc115eecb43fd53a96
Olof Bjarnason <olof.bjarnason@gmail.com>
2022-06-20 15:28:50 +0200

    wip

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+import {parse} from "./parser.js";
+
 async function setText(fileHandle) {
     let fileData = await fileHandle.getFile()
-    document.getElementById("editor").innerText = await fileData.text()
+    let source = await fileData.text();
+    const ast = parse(source)
+    const asJSON = JSON.stringify(ast, null, 2)
+    document.getElementById("editor").innerText = asJSON
 }
+
 async function getFile() {
     try {
         let [fileHandle] = await window.showOpenFilePicker()
