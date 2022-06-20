4da543071bc2ce127319def28e86319d1798a4d3
Olof Bjarnason <olof.bjarnason@gmail.com>
2022-06-20 15:05:29 +0200

    Fix warnings

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 async function setText(fileHandle) {
     let fileData = await fileHandle.getFile()
-    let text = await fileData.text()
-    document.getElementById("editor").innerText = text
+    document.getElementById("editor").innerText = await fileData.text()
 }
 async function getFile() {
     try {
         let [fileHandle] = await window.showOpenFilePicker()
         await setText(fileHandle)
         setInterval(() => { setText(fileHandle).then(() => console.log("updated"))}, 1000)
-    } catch (e) {
-        return
-    }
+    } catch (e) { }
 }
