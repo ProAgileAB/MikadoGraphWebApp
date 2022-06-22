d49c509d60ca5c83ed0b1b1edd95ce9fef613a4b
neppord <neppord@gmail.com>
2022-06-22 13:35:20 +0200

    Make the change easy

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 import {parse} from "./parser.js";
 
-async function setText(fileHandle) {
+function setInnerText(resultText) {
+  document.getElementById("editor").innerText = resultText
+}
+
+async function setText(fileHandle, callback) {
     let fileData = await fileHandle.getFile()
     let source = await fileData.text();
     const ast = parse(source)
-    document.getElementById("editor").innerText =
-      JSON.stringify(ast, null, 2)
+    let resultText = JSON.stringify(ast, null, 2)
+    callback(resultText)
 }
 
 export async function getFile() {
     try {
         let [fileHandle] = await window.showOpenFilePicker()
-        await setText(fileHandle)
-        setInterval(() => { setText(fileHandle).then(() => console.log("updated"))}, 1000)
+        setInterval(() => {
+            setText(fileHandle, setInnerText)
+               .then(() => console.log("updated"))
+        }, 500)
     } catch (e) { }
 }
