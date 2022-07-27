3415456ab269a39ee778bcb1cc1f006b09dfc846
neppord <neppord@gmail.com>
2022-07-27 10:57:12 +0200

    Only parse and render fi file content is different
    Co-authored-by: Olof Bjarnason <olof.bjarnason@proagile.se>

--- index.js
+++ index.js
@@ -5,13 +5,17 @@ function setInnerText(resultText) {
   document.getElementById("editor").innerText = resultText
 }
 
+let cacheSource = ""
 async function setText(fileHandle, callback) {
-    let fileData = await fileHandle.getFile()
-    const fileName = fileData.name
-    document.title = fileName
-    let source = await fileData.text()
+    const fileData = await fileHandle.getFile()
+    document.title = fileData.name
+    const source = await fileData.text()
+
+    if (cacheSource === source) return
+
+    cacheSource = source
     const ast = parse(source)
-    let resultText = translate(ast)
+    const resultText = translate(ast)
     callback(resultText)
 }
 
